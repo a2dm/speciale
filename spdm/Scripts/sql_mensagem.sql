b0715d61272b6e6220177f0cecfc4ffc4a2bfe0b
diego <cdiego.lima@gmail.com>
2016-12-07 22:11:47 -0300

    Script de mensagem

--- spdm/Scripts/sql_mensagem.sql
+++ spdm/Scripts/sql_mensagem.sql
@@ -26,3 +26,60 @@ WITH (
 );
 ALTER TABLE ped.tb_mensagem
   OWNER TO postgres;
+
+  
+  
+  
+  -- Table: ped.tb_mensagem_destinatario
+
+-- DROP TABLE ped.tb_mensagem_destinatario;
+
+CREATE TABLE ped.tb_mensagem_destinatario
+(
+  id_mensagem_destinatario bigint NOT NULL,
+  id_mensagem bigint NOT NULL,
+  id_cliente bigint,
+  CONSTRAINT pk_mensagem_destinatario PRIMARY KEY (id_mensagem_destinatario),
+  CONSTRAINT fk_mensagem_destinatario_cliente FOREIGN KEY (id_cliente)
+      REFERENCES ped.tb_cliente (id_cliente) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT fk_mensagem_destinatario_mensagem FOREIGN KEY (id_mensagem)
+      REFERENCES ped.tb_mensagem (id_mensagem) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE ped.tb_mensagem_destinatario
+  OWNER TO postgres;
+
+  
+  
+  
+  -- Sequence: ped.sq_mensagem
+
+-- DROP SEQUENCE ped.sq_mensagem;
+
+CREATE SEQUENCE ped.sq_mensagem
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 1
+  CACHE 1;
+ALTER TABLE ped.sq_mensagem
+  OWNER TO postgres;
+
+  
+  
+  -- Sequence: ped.sq_mensagem_destinatario
+
+-- DROP SEQUENCE ped.sq_mensagem_destinatario;
+
+CREATE SEQUENCE ped.sq_mensagem_destinatario
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 2
+  CACHE 1;
+ALTER TABLE ped.sq_mensagem_destinatario
+  OWNER TO postgres;
